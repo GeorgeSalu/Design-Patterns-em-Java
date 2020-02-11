3ffe139b0f0d59dca03304e12957bad5fae4caae
George Salu <george.salu10@gmail.com>
2020-02-11 14:29:16 -0300

    prototype

--- src/br/com/criacao/prototype/TestaPrototype.java
+++ src/br/com/criacao/prototype/TestaPrototype.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class TestaPrototype {
     public static void main(String[] args) {
+
         String nome = "k19";
 
         Calendar vencimento = Calendar.getInstance();
