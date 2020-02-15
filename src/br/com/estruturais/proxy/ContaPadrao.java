eda11bd2375e92e29ba810288efbb33b23639e39
George Salu <george.salu10@gmail.com>
2020-02-15 16:01:21 -0300

    proxy

---
+++ src/br/com/estruturais/proxy/ContaPadrao.java
@@ -0,0 +1,20 @@
+package br.com.estruturais.proxy;
+
+public class ContaPadrao implements Conta {
+    private double saldo;
+
+    @Override
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void saca(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public double getSaldo() {
+        return this.saldo;
+    }
+}
---
+++ src/br/com/estruturais/proxy/ContaProxy.java
@@ -0,0 +1,29 @@
+package br.com.estruturais.proxy;
+
+public class ContaProxy implements Conta {
+    private Conta conta;
+
+    public ContaProxy(Conta conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public void deposita(double valor) {
+        System.out.println("Efetuando o deposito de R$ "+valor+"...");
+        this.conta.deposita(valor);
+        System.out.println("Deposito de R$ "+valor+" efetuado ...");
+    }
+
+    @Override
+    public void saca(double valor) {
+        System.out.println("Efetuando o saque de R$ "+valor);
+        this.conta.saca(valor);
+        System.out.println("Saque de R$ "+valor+" efetuado.");
+    }
+
+    @Override
+    public double getSaldo() {
+        System.out.println("Verificando o saldo");
+        return this.conta.getSaldo();
+    }
+}
